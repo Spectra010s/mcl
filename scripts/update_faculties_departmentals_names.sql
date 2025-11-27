99ee7604834944e5e745d7da3e2acbd20d848c61
Spectra010s <adeloyeadetayo8@gmail.com>
2025-11-27 01:42:57 +0100

    chore: update latest migration sql to use different ALTER TABLES

--- scripts/update_faculties_departmentals_names.sql
+++ scripts/update_faculties_departmentals_names.sql
@@ -1,7 +1,13 @@
+-- One cant use comma after a SQL command except for ADD Column consecutively, so a command and its ALTER TABLE ---
+
 ALTER TABLE faculties
-RENAME COLUMN name TO short_name,
-ADD COLUMN full_name VARCHAR(255) UNIQUE NOT NULL;
+  RENAME COLUMN name TO short_name;
+
+ALTER TABLE faculties
+  ADD COLUMN full_name VARCHAR(255) UNIQUE NOT NULL;
 
 ALTER TABLE departments
-RENAME COLUMN name TO short_name,
-ADD COLUMN full_name VARCHAR(255) UNIQUE NOT NULL;
+  RENAME COLUMN name TO short_name;
+
+ALTER TABLE departments
+  ADD COLUMN full_name VARCHAR(255) UNIQUE NOT NULL;
