5b835cc81d261a5006cd9f1face7248cf556a19d
Spectra010s <adeloyeadetayo8@gmail.com>
2026-02-15 14:21:01 -0800

    fix: include faculty uniqueness removal in migration for future school support

--- scripts/15-2-2026-allow-duplicate-general-courses.sql
+++ scripts/15-2-2026-allow-duplicate-general-courses.sql
@@ -1,2 +1,4 @@
 ALTER TABLE departments DROP CONSTRAINT IF EXISTS departments_short_name_key;
 ALTER TABLE departments DROP CONSTRAINT IF EXISTS departments_full_name_key;
+ALTER TABLE faculties DROP CONSTRAINT IF EXISTS faculties_short_name_key;
+ALTER TABLE faculties DROP CONSTRAINT IF EXISTS faculties_full_name_key;
